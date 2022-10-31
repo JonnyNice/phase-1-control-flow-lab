01268f3c1b7819653a747b4303cd82e414d0361c
JonnyNice <116230053+JonnyNice@users.noreply.github.com>
2022-10-31 15:18:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(poolDepth){
+  let rideCost
+    if (poolDepth <= 400)
+      {rideCost = 'This one is on me!'}
+      else if (poolDepth < 2000) 
+        {rideCost = 'That will be twenty bucks.'}
+          else if (poolDepth <= 2500)
+            {rideCost = 'I will gladly take your thirty bucks.'} 
+              else if (poolDepth >= 2501)
+               {rideCost = 'No can do.'} 
+  return rideCost
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response;
+  city === "NYC" ? response = "Ok, sounds good." : response = "No go.";
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
